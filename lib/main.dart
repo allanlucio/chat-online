7164c3604f4ca5df3739d6d9abf403b5c76c5472
allan <luciocorreia.allan@gmail.com>
2020-04-07 11:24:27 -0300

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  
+  runApp(MyApp());
+  Firestore.instance.collection("col").document("doc").setData({"Texto":"daniel"});
+} 
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
